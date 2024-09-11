bc888b11e528675908fcdbbed20d51b393276aa3
Ginger Society Admin <admin@gingersociety.org>
2024-09-11 13:15:08 +0530

    feat: updated styles for the installation info component

--- src/components/products/installation-info.jsx
+++ src/components/products/installation-info.jsx
@@ -1,34 +1,29 @@
-import { FaRegCopy, FaTerminal } from "react-icons/fa";
+import { FaRegCopy } from "react-icons/fa";
+import styles from './installation-info.module.scss';
 
 const InstallationInfo = () => {
+  const commandText = `bash -c "$(curl -fsSL https://raw.githubusercontent.com/ginger-society/infra-as-code-repo/main/rust-helpers/install-all-clis.sh)"`;
 
   const handleCopy = () => {
-    const command = `bash -c "$(curl -fsSL https://raw.githubusercontent.com/ginger-society/infra-as-code-repo/main/rust-helpers/install-all-clis.sh)"`;
-    navigator.clipboard.writeText(command).then(() => {
+    navigator.clipboard.writeText(commandText).then(() => {
       alert("Command copied to clipboard!");
     }).catch(err => {
       console.error("Failed to copy command", err);
     });
-  }
+  };
 
   return (
-    <div
-      style={{
-        background: "black",
-        display: "flex",
-        gap: '20px',
-        flexDirection: 'column',
-        color: "white",
-        padding: "20px",
-        alignItems: "center",
-      }}
-    >
-      <span>Get started by installing our dev tools, Copy the command below and paste it in your terminal ( use sudo mode if needed ) </span>
-      <span style={{ margin: 'auto', display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px', fontSize: '18px', border: 'solid 0.5px white', padding: '20px', borderRadius: '20px' }}>
-        <span style={{ color: '#e53b2a' }}>$</span> bash -c "$(curl -fsSL
-        https://raw.githubusercontent.com/ginger-society/infra-as-code-repo/main/rust-helpers/install-all-clis.sh)"  </span>
-      <span onClick={handleCopy} style={{ fontSize: '15px', cursor: 'pointer' }}>Copy <FaRegCopy /></span>
-
+    <div className={styles.container}>
+      <span>
+        Get started by installing our dev tools. Copy the command below and paste it in your terminal (use sudo mode if needed).
+      </span>
+      <span className={styles.command}>
+        <span className={styles.dollarSign}>$</span>
+        <span className={styles.typedCommand}>{commandText}</span>
+      </span>
+      <span onClick={handleCopy} className={styles.copyButton}>
+        Copy <FaRegCopy />
+      </span>
     </div>
   );
 };
